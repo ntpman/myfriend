ad413486c80e0efd382f80913195da8b19410df5
ntpman <kpntpman@gmail.com>
2023-05-09 15:56:37 +0700

    update code

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Profile;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('profile.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:255|unique:profiles',
+            'tel' => 'required|max:255',
+            'email' => 'required|max:255|email|unique:profiles',
+            'detail' => 'required',
+        ]);
+
+        $profile = new Profile();
+        $profile->name = $request->name;
+        $profile->tel = $request->tel;
+        $profile->email = $request->email;
+        $profile->detail = $request->detail;
+        $profile->save();
+
+        return redirect()->route('profile.index');
+    }
 }
